51aea7e016cceb2609c7b44531f1221813bce097
Ben <52283112+user-benjamin@users.noreply.github.com>
2023-07-12 07:58:04 -0400

    Update list_ecr.py

--- list_ecr.py
+++ list_ecr.py
@@ -26,3 +26,44 @@ for repo in repos:
         # print('Size: ' + str(image['imageSizeInBytes']) + ' bytes')
         # print('Upload date: ' + str(image['imagePushedAt']))
         print(f"{repo_name},{image['imageTags'][0]},{str(image['imageSizeInBytes'])}")
+
+
+
+###sendToCSV###
+
+# import boto3
+# import csv
+
+# # Create an ECR client with a specific region
+# ecr = boto3.client('ecr', region_name='us-east-1')
+
+# # Get a list of all ECR repositories
+# repos = ecr.describe_repositories()['repositories']
+
+# # Create a CSV writer object
+# csv_writer = csv.writer(open('output.csv', 'w'))
+
+# # Write the header row to the CSV file
+# csv_writer.writerow(['Repository', 'Image name', 'Size', 'Upload date'])
+
+# # Loop through each repository and list its images
+# for repo in repos:
+#     # Get the repository name
+#     repo_name = repo['repositoryName']
+    
+#     # Get a list of all images in the repository
+#     images = ecr.describe_images(repositoryName=repo_name)['imageDetails']
+    
+#     # Loop through each image and write its data to the CSV file
+#     for image in images:
+#         try:
+#             # Get the image name and tags
+#             image_name = ','.join(image['imageTags'])
+#         except KeyError:
+#             # Handle the KeyError for untagged images
+#             image_name = '<untagged>'
+#         size = image['imageSizeInBytes']
+#         upload_date = image['imagePushedAt']
+        
+#         # Write the data row to the CSV file
+#         csv_writer.writerow([repo_name, image_name, size, upload_date])
